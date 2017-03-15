cf8edee246c8834aa21acee707a1f789b87a0a83
ed42311 <ed42311@gmail.com>
2017-03-14 21:39:34 -0600

    goes through all three questions

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var clicked = false;
+var data = [
+	{question: "Who makes it, has no need of it.\nWho buys it, has no use for it.\nWho uses it can neither see nor feel it.\nWhat is it?", answer:"a coffin"},
+	{question: "What kind of room has no doors or windows?", answer:"a mushroom"},
+	{question: "Which creature walks on four legs in the morning,\n two legs in the afternoon, and\n three legs in the evening?", answer:"Man"}
+];
+var index = 0;
 document.getElementById("btn1").addEventListener('click', function(e){
-	if(clicked == false){
+	  var riddle = document.getElementById("riddle");
+		if(riddle !== null) riddle.parentNode.removeChild(riddle);
 		var h2 = document.createElement("h2");
-		h2.innerText = "hello";
+		h2.innerText = data[index].question;
+		h2.setAttribute("id", "riddle");
 		document.body.append(h2);
-	}
-	clicked = true;
+		index++;
 });
